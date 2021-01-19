d1917d2fab4f060760e3893abca2ec33276f2a14
makidelmondo <wasichkaufe@hotmail.co.jp>
2021-01-19 18:24:18 +0900

    購入画面参照不可条件の追加

--- app/controllers/deal_records_controller.rb
+++ app/controllers/deal_records_controller.rb
@@ -1,5 +1,7 @@
 class DealRecordsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_product, only: [:index, :new, :create]
+  before_action :move_to_index
   def index
     @deal_record = DealRecord.new
   end
@@ -16,7 +18,7 @@ class DealRecordsController < ApplicationController
   end
 
   private
-
+#定義したメソッドの実行順はプライベートメソッドないの順序ではなくビフォーアクションの順序に準ずると留意すること。
   def deal_params
     params.require(:deal_record).permit(:zip_code, :state_id, :city, :street, :apartment, :telephone).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])
   end
@@ -32,5 +34,11 @@ class DealRecordsController < ApplicationController
 
   def set_product
     @product = Product.find(params[:product_id])
+  end
+  
+  def move_to_index
+    if current_user.id == @product.user_id || @product.deal_record.present?
+      redirect_to root_path
+    end
   end
 end
